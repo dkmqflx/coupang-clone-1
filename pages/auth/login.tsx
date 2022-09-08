912de8fdb1fd5dede5e49a2735a87579bf0ed6e2
Dohyun Kim <dkmqflx@gmail.com>
2022-09-08 23:56:43 +0900

    feat: add login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -26,42 +26,41 @@ export default function LoginPage() {
   };
 
   return (
-    <Wrapper>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* <input {...register("email")} /> */}
-        {/* <input {...register("password")} /> */}
-        <Input
-          type="email"
-          {...register("email", {
-            required: "아이디를 입력해주세요",
-            pattern: {
-              value: /^\S+@\S+\.[a-zA-Z]{2,3}$/i,
-              message: "아이디(이메일)는 이메일 형식으로 입력해주세요.",
-            },
-          })}
-          errorMessage={errors.email?.message}
-          placeholder="아이디(이메일)"
-          icon={MdOutlineEmail}
-        />
-        <Input
-          type="password"
-          {...register("password", {
-            required: "비밀번호를 입력해주세요",
-          })}
-          errorMessage={errors.password?.message}
-          placeholder="비밀번호"
-          icon={MdLockOutline}
-        />
+    <FormWrapper onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        type="email"
+        {...register("email", {
+          required: "아이디를 입력해주세요",
+          pattern: {
+            value: /^\S+@\S+\.[a-zA-Z]{2,3}$/i,
+            message: "아이디(이메일)는 이메일 형식으로 입력해주세요.",
+          },
+        })}
+        errorMessage={errors.email?.message}
+        placeholder="아이디(이메일)"
+        icon={MdOutlineEmail}
+      />
+      <Input
+        type="password"
+        {...register("password", {
+          required: "비밀번호를 입력해주세요",
+        })}
+        errorMessage={errors.password?.message}
+        placeholder="비밀번호"
+        icon={MdLockOutline}
+      />
 
-        {/* <button type="submit">로그인</button> */}
-        <Button primary>로그인</Button>
-        {/* <a href="/auth/signup">회원가입</a> */}
-        <Link href="/auth/signup" passHref>
-          <Button>회원가입</Button>
-        </Link>
-      </form>
-    </Wrapper>
+      <Button primary>로그인</Button>
+      <Link href="/auth/signup" passHref>
+        <Button>회원가입</Button>
+      </Link>
+    </FormWrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const FormWrapper = styled.form`
+  margin-top: 100px;
+  & > div {
+    margin: 0 auto;
+  }
+`;
