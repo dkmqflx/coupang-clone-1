cac1f05eda13d6836e91f40ae62dcfbb3543f85c
Dohyun Kim <dkmqflx@gmail.com>
2022-10-19 11:52:15 +0900

    feat(): change CheckBox stories

--- src/components/common/CheckBox/CheckBox.stories.tsx
+++ src/components/common/CheckBox/CheckBox.stories.tsx
@@ -6,24 +6,33 @@ export default {
   component: CheckBox,
 } as ComponentMeta<typeof CheckBox>;
 
-export const agreementAll: ComponentStory<typeof CheckBox> = () => (
+export const Bold: ComponentStory<typeof CheckBox> = () => (
   <CheckBox
     text='모두 확인하였으며 동의합니다.'
     subText='동의에는 필수 및 선택 목적(광고성 정보 수신 포함)에 대한 동의가
 포함되어 있으며, 선택 목적의 동의를 거부하시는 경우에도 서비스 이용이
 가능합니다.'
-    checked
+    checked={false}
+    bold
   ></CheckBox>
 );
 
-export const nonDescription: ComponentStory<typeof CheckBox> = () => (
-  <CheckBox text='[필수] 만 14세 이상입니다' checked={false}></CheckBox>
+export const Checked: ComponentStory<typeof CheckBox> = () => (
+  <CheckBox text='[필수] 만 14세 이상입니다' checked={true}></CheckBox>
 );
 
-export const description: ComponentStory<typeof CheckBox> = () => (
+export const Description: ComponentStory<typeof CheckBox> = () => (
   <CheckBox
     text='[선택] 광고성 정보 수신 동의'
     checked={false}
     description
   ></CheckBox>
+);
+
+export const CheckBoxGroup: ComponentStory<typeof CheckBox> = () => (
+  <CheckBox checked={false} description text='[선택] 광고성 정보 수신 동의'>
+    <CheckBox checked={false} text='[선택] 이메일 수신 동의' />
+    <CheckBox checked={false} text='[선택] SMS,MMS 수신 동의' />
+    <CheckBox checked={false} text='[선택] 앱 푸시 수신 동의' />
+  </CheckBox>
 );
