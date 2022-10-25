9c929facdfbf04429d0f10e71cbfcadede5a0e4a
Dohyun Kim <dkmqflx@gmail.com>
2022-10-25 13:46:09 +0900

    feat(): add api for other products

--- pages/api/products/[productId]/vendoritems/[vendoritemId].ts
+++ pages/api/products/[productId]/vendoritems/[vendoritemId].ts
@@ -16,7 +16,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    console.log(one);
     res.status(200).json({
       images: {
         main,
